e80358f788dc52600e8f30d0338ea041c84cfbdd
Nickrhys <nickrhys@hotmail.com>
2017-03-25 09:11:38 +0000

    - skeleton haml, sass and jQuery for dropdown menu - disable sass caching

--- config.ru
+++ config.ru
@@ -6,6 +6,8 @@ require './app'
 
 # use scss for stylesheets https://andrew.stwrt.ca/posts/sinatra-coffeescript-sass/
 Sass::Plugin.options[:style] = :compressed
+Sass::Plugin.options[:cache] = false
 use Sass::Plugin::Rack
+
 
 run App
--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,8 +1,5 @@
 $(document).ready(function(){
-    $("#hide").click(function(){
-        $("h1").hide();
-    });
-    $("#show").click(function(){
-        $("h1").show();
+    $(".avatar").click(function(){
+        $("#dropdown").toggle();
     });
 });
